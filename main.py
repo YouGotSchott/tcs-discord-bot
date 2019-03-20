9cfbfc3b39ecc638c47a72bbb19bdeec9eb962e3
Lucas Schott <lucschott@gmail.com>
2019-03-20 12:50:49 -0400

    Added !tism command

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 from com.pbo_downloader import PBODownloader
-from secret import client, TOKEN, main_path, ww2_path
+from com.data.cues import cue_message
+from config import client, TOKEN, main_path, ww2_path
 
 client.remove_command('help')
 
@@ -17,15 +18,19 @@ async def uploadww2(ctx):
     pbo = PBODownloader()
     await pbo.downloader(ctx, ww2_path)
 
+@client.command(pass_context=True)
+async def tism(ctx):
+    await client.send_message(ctx.message.channel, cue_message)
+
 @client.event
 async def on_message(message):
     if client.user in message.mentions:
         await client.add_reaction(message, '\U0001f5de')
     msg = str(message.content)
     msg = msg.replace(" ", "")
-    if msg.startswith('<:') and \
-        msg.endswith('>') and \
-        message.author.id == '206380629523300352':
+    if message.author.id == '206380629523300352' \
+        and msg.startswith('<:') \
+        and msg.endswith('>'):
         await client.delete_message(message)
     await client.process_commands(message)
 
