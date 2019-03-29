d3bda6c42f536d332587c0ae968165cb5a0d7d22
Lucas Schott <lucschott@gmail.com>
2019-03-29 10:09:46 -0400

    Changed formatting for "!amazon" embed

--- com/data/amazon_embed.py
+++ com/data/amazon_embed.py
@@ -3,7 +3,7 @@ from config import client
 
 msg_description = '''
 🇺🇸 **US MEMEBERS ONLY** 🇺🇸
-Use the [Amazon Affiliate Link](https://www.amazon.com/?&_encoding=UTF8&tag=thecoolerse0c-20&linkCode=ur2&linkId=29b1e7ab218e7b0ef5e05f03199eb2b7&camp=1789&creative=9325) on your next purchase and the server recieves a percentage of the sale **AT NO COST TO YOU!** Just make sure you *click the link* at least **24hrs before you check out**.
+Use the [Amazon Affiliate Link](https://www.amazon.com/?&_encoding=UTF8&tag=thecoolerse0c-20&linkCode=ur2&linkId=29b1e7ab218e7b0ef5e05f03199eb2b7&camp=1789&creative=9325) on your next purchase and the server recieves a percentage of the sale **AT NO COST TO YOU!** Make sure you *click the link* at least **24hrs before you check out**.
 '''
 
 amazon_embed = discord.Embed(title='Amazon Affiliate Link', description=msg_description, url='https://www.amazon.com/?&_encoding=UTF8&tag=thecoolerse0c-20&linkCode=ur2&linkId=29b1e7ab218e7b0ef5e05f03199eb2b7&camp=1789&creative=9325', color=0xff9900)
