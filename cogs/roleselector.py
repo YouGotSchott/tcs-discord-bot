578d295c8f6aa265aab21e85753d62cedf4e54b9
Lucas Schott <lucschott@gmail.com>
2019-04-03 16:01:03 -0400

    Laid framework for RoleSelector

--- cogs/roleselector.py
+++ cogs/roleselector.py
@@ -1,80 +1,53 @@
 import discord
-from discord.ext import commands
+from pathlib import Path
+import json
 
-msgs = []
+# role_msg = str(Path('data/role_msg.json'))
 
-titles = [
-    '@mission-maker',
-    '@minecraft',
-    '@eco'
-]
-descriptions = [
-    '''
+msg_embed = {
+    'title' : '@mission-maker',
+    'description' : '''
     This role gives access to the mission making channels. Everyone is free to give mission making a try.
     **__Requirements__**
     *> Attend at least 1 Saturday Op*
     *> Understand that we make missions differently than other units*
     ''',
-    '''
-    Allows other people who play Minecraft to ping your role directly.
-    ''',
-    '''
-    Allows other people who play Eco to ping you about how there isn't a server anymore.
-    '''
-]
-names = [
-    'Mission Making',
-    'Minecraft',
-    'Eco'
-]
-
+    'name' : 'Mission Making',
+}
 
 class RoleSelector:
     def __init__(self, client):
         self.client = client
+    
+    async def on_ready(self):
+        channel = self.client.get_channel('557248486324699176')
+        text = await self.embeder(msg_embed)
+        msg = await self.client.send_message(channel, embed=text)
+        # msg_id = {'id' : msg}
+        await self.client.add_reaction(msg, emoji='👍')
 
-    async def on_reaction_add(self, reaction, ctx):
-        roleChannelId = '557248486324699176'
-        if reaction.message.channel.id != roleChannelId:
+    async def on_reaction_add(self, reaction, user):
+        channel = self.client.get_channel('557248486324699176')
+        if 'roles' != str(channel):
             return
         if reaction.emoji == "👍":
-            user_id = ctx.reaction.server.get_member(ctx.message.author.id)
-            await self.client.add_roles(user_id, '558351518831607831')
+            role = discord.utils.get(user.server.roles, name="test")
+            await self.client.add_roles(user, role)
+    
+    async def on_reaction_remove(self, reaction, user):
+        channel = self.client.get_channel('557248486324699176')
+        if 'roles' != str(channel):
+            return
+        if reaction.emoji == "👍":
+            role = discord.utils.get(user.server.roles, name="test")
+            await self.client.remove_roles(user, role)
 
-    @commands.command(pass_context=True)
-    @commands.has_any_role('admin')
-    async def embed(self, ctx):
-        await self.client.delete_message(ctx.message)
-        await self.rotation(ctx, msgs)
-
-    async def post(self, ctx, msg_data):
+    async def embeder(self, msg_embed):
         em = discord.Embed(
-            title=msg_data['title'], description=msg_data['description'], colour=0xDEADBF)
-        em.set_author(name=msg_data['name'],
+            title=msg_embed['title'], description=msg_embed['description'], colour=0xDEADBF)
+        em.set_author(name=msg_embed['name'],
                       icon_url=self.client.user.avatar_url)
-        msg = await self.client.send_message(self.client.get_channel('557248486324699176'), embed=em)
-        await self.client.add_reaction(msg, '👍')
-        return msg
-
-    async def dict_creator(self, title, description, name):
-        msg_data = {
-            'title': title,
-            'description': description,
-            'name': name
-        }
-        return msg_data
-
-    async def remove(self, msgs):
-        if msgs:
-            await self.client.delete_messages(msgs)
-
-    async def rotation(self, ctx, msgs):
-        await self.remove(msgs)
-        embed_msgs = zip(titles, descriptions, names)
-        for title, description, name in embed_msgs:
-            msg_data = await self.dict_creator(title, description, name)
-            msg = await self.post(ctx, msg_data)
-            msgs.append(msg)
+        return em
 
 
 def setup(client):
