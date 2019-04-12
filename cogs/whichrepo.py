0ee7537e2e630f504d9616d5291983789b36792e
Lucas Schott <yougotschott@gmail.com>
2019-04-12 19:10:18 -0400

    Corrected days of week for "whichrepo"

--- cogs/whichrepo.py
+++ cogs/whichrepo.py
@@ -21,7 +21,7 @@ class WhichRepo:
         msg = message.content.lower()
         if ('which repo' in msg \
         or 'what repo' in msg) \
-        and datetime.now(timezone('US/Eastern')).weekday() in [2, 5, 6]:
+        and datetime.now(timezone('US/Eastern')).weekday() in [2, 4, 5]:
             if str(message.author.id) == '188724792680120320':
                 await self.client.send_message(message.channel, 
                 "{} We're using the main repo you {}.".format(dummy, 'crayon eater'))
