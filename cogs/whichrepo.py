c1647909f48f8811d4f62e32d1cccafe7a19183c
Lucas Schott <lucschott@gmail.com>
2019-04-16 14:32:56 -0400

    Added option for alternate repo

--- cogs/whichrepo.py
+++ cogs/whichrepo.py
@@ -11,6 +11,18 @@ class WhichRepo(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self, message):
+        msg = message.content.lower()
+        if ('which repo' in msg \
+        or 'what repo' in msg):
+            if datetime.now(timezone('US/Eastern')).weekday() in [4, 5]:
+                self.insult(message, 'main')
+            elif datetime.now(timezone('US/Eastern')).weekday() == 2:
+                self.insult(message, 'WW2')
+            else:
+                await message.add_reaction('\U0001f5de')
+    
+    async def insult(self, message, repo):
+        dummy = message.author.mention
         insults = [
             'big fucking idiot',
             'satchel of assholes',
@@ -19,18 +31,12 @@ class WhichRepo(commands.Cog):
             'fucking sitzpinkler',
             'bumbling sycophant'
         ]
-        dummy = message.author.mention
-        msg = message.content.lower()
-        if ('which repo' in msg \
-        or 'what repo' in msg) \
-        and datetime.now(timezone('US/Eastern')).weekday() in [2, 4, 5]:
-            if str(message.author.id) == 188724792680120320:
-                await message.send(
-                    "{} We're using the main repo you {}.".format(dummy, 'crayon eater'))
-            else:
-                await message.send(
-                    "{} We're using the main repo you {}.".format(dummy, choice(insults)))
-
+        if message.author.id == 188724792680120320:
+            await message.send(
+                "{} We're using the {} repo you {}.".format(dummy, repo, 'crayon eater'))
+        else:
+            await message.send(
+                "{} We're using the {} repo you {}.".format(dummy, repo, choice(insults)))
 
 def setup(bot):
     bot.add_cog(WhichRepo(bot))
