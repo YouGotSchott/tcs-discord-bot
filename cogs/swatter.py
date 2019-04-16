bb192b7f70d41994e670ad7fec8d66b04b87908c
Lucas Schott <lucschott@gmail.com>
2019-04-16 14:33:20 -0400

    Swatter will now stack newspaper emojis

--- cogs/swatter.py
+++ cogs/swatter.py
@@ -10,6 +10,16 @@ class Swatter(commands.Cog):
     async def on_message(self, message):
         if self.bot.user in message.mentions:
             await message.add_reaction('\U0001f5de')
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.user_id == self.bot.user.id:
+            return
+        if str(payload.emoji) == '\U0001f5de':
+            guild = self.bot.get_guild(payload.guild_id)
+            channel = guild.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            await message.add_reaction('\U0001f5de')
 
 
 def setup(bot):
