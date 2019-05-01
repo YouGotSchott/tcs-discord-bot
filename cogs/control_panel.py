12e7bf3a328642907777898d54b657de4966fd1a
Lucas Schott <lucschott@gmail.com>
2019-05-01 16:00:46 -0400

    Continued work on Control Panel

--- cogs/control_panel.py
+++ cogs/control_panel.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from pathlib import Path
+import json
 
 
 class ControlPanel(commands.Cog):
@@ -10,7 +11,7 @@ class ControlPanel(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
+        await self.msg_setup()
 
     async def opener(self):
         with open(self.panel, 'r') as f:
@@ -21,21 +22,80 @@ class ControlPanel(commands.Cog):
             json.dump(panel, f)
 
     async def msg_setup(self):
-        channel = discord.utils.get(self.bot.get_all_channels(), name='control-panel')
-        text = await self.embeder(emojis)
         panel = await self.opener()
-        try:
-            self.arma = await channel.fetch_message(panel.get('arma'))
-            self.arma_hc = await channel.fetch_message(panel.get('arma_hc'))
-            self.ww2 = await channel.fetch_message(panel.get('ww2'))
-            self.ww2_hc = await channel.fetch_message(panel.get('ww2_hc'))
-            await self.msg.edit(embed=text)
-        except:
-            print("Role Message hasn't been added yet")
-            self.msg = await channel.send(embed=text)
+        channel = discord.utils.get(self.bot.get_all_channels(), name='control-panel')
+        emojis = ['start', 'stop', 'restart', 'validate', 'update']
+        server = {
+            'arma' : {
+                'title' : 'ARMA 3 SERVER',
+                'url' : 'https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2019_logo.png'
+            },
+            'arma_hc' : {
+                'title' : 'ARMA 3 HEADLESS CLIENT',
+                'url' : 'https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2019_logo.png'
+            },
+            'ww2' : {
+                'title' : 'ARMA 3 WW2 SERVER',
+                'url' : 'https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2019_logo.png'
+            },
+            'ww2_hc' : {
+                'title' : 'ARMA 3 WW2 SERVER',
+                'url' : 'https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2019_logo.png'
+            },
+            'minecraft' : {
+                'title' : 'MINECRAFT',
+                'url' : 'https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2019_logo.png'
+            }
+        }
+        for server_value, key, value in zip(server.values(), panel.keys(), panel.values()):
+            print(server_value)
+            print(key)
+            print(value)
+            text = await self.embeder(server_value['title'], server_value['url'], emojis)
+            panel = await self.opener()
+            try:
+                msg = await channel.fetch_message(panel.get(value))
+                await msg.edit(embed=text)
+            except:
+                print("Control panel message hasn't been made yet.")
+                msg = await channel.send(embed=text)
+                msg = panel[key]
+                # for emoji in emojis:
+                #     await msg.add_reaction(emoji=emoji)
+        await self.closer(panel)
 
-    async def embeder(self, emojis):
-
+    async def embeder(self, server_title, server_url, emojis):
+        desc = 'Use the following reactions to perform actions on the server. Your reaction will be removed once the action has completed.'
+        foot = 'The bot will show as "typing" while the action is runnning.'
+        commands = {
+            'start' : {
+                'name' : "{} starts the server".format(emojis[0]),
+                'value' : ""
+            },
+            'stop' : {
+                'name' : "{} stops the server".format(emojis[1]),
+                'value' : ""
+            },
+            'restart' : {
+                'name' : "{} restarts the server.".format(emojis[2]),
+                'value' : "*Use when you're not sure if the server is stopped.*"
+            },
+            'validate' : {
+                'name' : "{} validates server files.".format(emojis[3]),
+                'value' : "*Used to troubleshoot the server files.*"
+            },
+            'update' : {
+                'name' : "{} updates the server.".format(emojis[4]),
+                'value' : ""
+            }
+        }
+        em = discord.Embed(
+            title=server_title, description=desc, color=0x008080)
+        em.set_thumbnail(url=server_url)
+        for value in commands.values():
+            em.add_field(name=value['name'], value=value['value'], inline=True)
+        em.set_footer(text=foot)
+        return em
 
 def setup(bot):
     bot.add_cog(ControlPanel(bot))
