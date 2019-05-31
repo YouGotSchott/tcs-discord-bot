cd2d1aeafb492b507fadda38c0ff3e6131dad392
Lucas Schott <lucas@MacBook-Pro.local>
2019-05-31 15:18:31 -0400

    Added temporary !joined command

--- cogs/howtall.py
+++ cogs/howtall.py
@@ -8,7 +8,18 @@ class HowTall(commands.Cog):
 
     @commands.command()
     async def howtall(self, ctx):
+        still = [92362557439893504]
+        if ctx.message.author.id in still:
+            await ctx.send("__STILL__ not enough.")
         await ctx.send("Not enough.")
+
+    @commands.command()
+    async def joined(self, ctx):
+        guild = self.bot.get_guild(self.bot.guilds[0].id)
+        member = guild.get_member(ctx.message.author.id)
+        arrival = member.joined_at
+        arrival = arrival.strftime("%d %B, %Y")
+        await ctx.send(arrival)
 
 
 def setup(bot):
