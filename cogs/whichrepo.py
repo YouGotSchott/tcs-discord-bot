0187a4c4c81b7ab015c922087a71668d334bd9d1
YouGotSchott <yougotschott@gmail.com>
2019-06-14 13:31:37 -0400

    Added insults

--- cogs/whichrepo.py
+++ cogs/whichrepo.py
@@ -8,7 +8,7 @@ from pytz import timezone
 class WhichRepo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
         msg = message.content.lower()
@@ -20,7 +20,7 @@ class WhichRepo(commands.Cog):
                 await self.insult(message, 'WW2')
             else:
                 await message.add_reaction('\U0001f5de')
-    
+
     async def insult(self, message, repo):
         dummy = message.author.mention
         insults = [
@@ -29,14 +29,21 @@ class WhichRepo(commands.Cog):
             'absolute fucking dumpling',
             'cock weasel',
             'fucking sitzpinkler',
-            'bumbling sycophant'
+            'bumbling sycophant',
+            'leaky fuck faucet',
+            'ignorant ingrown sausage link',
+            'absolute pinecone',
+            'incompetent inkless pen',
+            'unintelligent park bench',
+            'dense cabbage',
+            'Canadian',
+            'badly packed parachute'
         ]
         if message.author.id == 188724792680120320:
-            await message.channel.send(
-                "{} We're using the {} repo you {}.".format(dummy, repo, 'crayon eater'))
-        else:
-            await message.channel.send(
-                "{} We're using the {} repo you {}.".format(dummy, repo, choice(insults)))
+            insults.append('crayon eater')
+        await message.channel.send(
+            "{} We're using the {} repo you {}.".format(dummy, repo, choice(insults)))
+
 
 def setup(bot):
     bot.add_cog(WhichRepo(bot))
