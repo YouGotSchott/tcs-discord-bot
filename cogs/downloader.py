48d87f6078736daf9c4a0bf0ce2da29ba6ed0e68
YouGotSchott <yougotschott@gmail.com>
2019-06-21 14:24:29 -0400

    Changed upload method to FTP

--- cogs/downloader.py
+++ cogs/downloader.py
@@ -1,33 +1,37 @@
 import discord
 from discord.ext import commands
 import aiohttp
-from config import main_path, ww2_path
+from config import ftp_host, ftp_user, ftp_pwd
 from pathlib import Path
+import ftplib
 
 
 class Downloader(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.cache = Path('cogs/data/pbo_cache/')
 
     @commands.command()
     @commands.has_any_role('upload', 'admin', 'moderator')
     async def upload(self, ctx):
-        await self.downloader(ctx, main_path)
+        self.folder = 'main_mpmissions'
+        await self.downloader(ctx)
 
     @commands.command()
     @commands.has_any_role('upload', 'admin', 'moderator')
     async def ww2upload(self, ctx):
-        await self.downloader(ctx, ww2_path)
+        self.folder = 'ww2_mpmissions'
+        await self.downloader(ctx)
 
-    async def downloader(self, ctx, file_path):
-        await self.status_check(ctx, await self.ingest(ctx, file_path))
+    async def downloader(self, ctx):
+        await self.status_check(ctx, await self.ingest(ctx))  
 
-    async def ingest(self, ctx, file_path):
+    async def ingest(self, ctx):
         url = ctx.message.attachments[0].url
         filename = url.split("/")
         if filename[-1].endswith('.pbo'):
-            end_file = Path(filename[-1])
-            path = str(file_path / end_file)
+            self.end_file = Path(filename[-1])
+            path = str(self.cache / self.end_file)
             attachment = {
                 "url": url,
                 "path": path
@@ -57,8 +61,18 @@ class Downloader(commands.Cog):
                 if not chunk:
                     break
                 f.write(chunk)
+            await self.ftp_to_server(path)
             await ctx.message.add_reaction('👍')
 
+    async def ftp_to_server(self, path):
+        ftp = ftplib.FTP(host=ftp_host)
+        ftp.login(user=ftp_user, passwd=ftp_pwd)
+        ftp.cwd(self.folder)
+        file = open(path,'rb')
+        filename = str(self.end_file)
+        ftp.storbinary("STOR {}".format(filename), file)
+        file.close()
+        ftp.quit()
 
 def setup(bot):
     bot.add_cog(Downloader(bot))
