568bc214fd9e04f1d1ecb2fae3531a8c4d142f57
YouGotSchott <yougotschott@gmail.com>
2019-11-11 20:28:58 -0500

    Removed Ham emotes

--- cogs/swatter.py
+++ cogs/swatter.py
@@ -12,9 +12,6 @@ class Swatter(commands.Cog):
             await message.add_reaction('\U0001f5de')
         if 'skynet' in message.content.lower():
             await message.add_reaction('\U0001f916')
-        if 'bitch' in message.content.lower() and \
-            message.author.id == 178960174738833409:
-            await message.add_reaction('its_just_ham:504456342119907349')
     
     @commands.Cog.listener(name='on_raw_reaction_add')
     async def newspaper_stacker(self, payload):
@@ -25,19 +22,6 @@ class Swatter(commands.Cog):
             channel = guild.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
             await message.add_reaction('\U0001f5de')
-
-    @commands.Cog.listener(name='on_raw_reaction_add')
-    async def ham_stacker(self, payload):
-        if payload.user_id == self.bot.user.id:
-            return
-        channel = self.bot.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
-        if str(payload.emoji).strip('<:>') == 'its_just_ham:504456342119907349' \
-            and message.author.id == 178960174738833409:
-            guild = self.bot.get_guild(payload.guild_id)
-            channel = guild.get_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
-            await message.add_reaction('its_just_ham:504456342119907349')
 
 
 def setup(bot):
