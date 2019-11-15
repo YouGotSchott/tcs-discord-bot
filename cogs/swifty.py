2b9b3859aa459da627a5dd23adc560f1ff9eaf13
YouGotSchott <yougotschott@gmail.com>
2019-11-15 15:36:01 -0500

    Added shortcut instructions for Swifty

--- cogs/swifty.py
+++ cogs/swifty.py
@@ -45,6 +45,19 @@ class Swifty(commands.Cog):
         em.add_field(name="**STEP 3**", value=guide['step 3'], inline=True)
         await ctx.send(embed=em)
 
+    @swifty.command()
+    async def shortcut(self, ctx):
+        guide = {
+            'description' : "```Swifty may not create a shortcut on your desktop, and while some people use the downloaded *Installation File* as a way to open Swifty, it is not recommended and could cause unintentional side-effects.```",
+            'step 1' : """Find the "Swifty.exe" at `%localappdata%\\app\\2.2.1\\` *(You can copy and paste this directory into "Run" `⊞Win + R` to find it quicker.*).""",
+            'step 2' : """Right-click "Swifty.exe" and choose "Send to Desktop"."""
+        }
+        em = discord.Embed(
+            title="How to Create a Desktop Shortcut for Swifty", description=guide['description'], color=0x29b585)
+        em.add_field(name="**STEP 1**", value=guide['step 1'], inline=True)
+        em.add_field(name="**STEP 2**", value=guide['step 2'], inline=True)
+        await ctx.send(embed=em)
+
 
 def setup(bot):
     bot.add_cog(Swifty(bot))
