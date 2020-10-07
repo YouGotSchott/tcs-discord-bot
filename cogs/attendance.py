f0226eb38253ab742517227497fc69c912f5f53b
YouGotSchott <yougotschott@gmail.com>
2020-10-07 16:05:05 +0000

    Added !nolife command

--- cogs/attendance.py
+++ cogs/attendance.py
@@ -5,13 +5,14 @@ from datetime import datetime
 from config import bot
 import asyncio
 
+
 class Attendance(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.toggle = False
 
     @commands.command()
-    @commands.has_any_role('admin', 'moderator')
+    @commands.has_any_role("admin", "moderator")
     async def startsignup(self, ctx):
         self.bot.fake_toggle = False
         self.toggle = True
@@ -21,7 +22,7 @@ class Attendance(commands.Cog):
         self.uid_list.clear()
 
     @commands.command()
-    @commands.has_any_role('admin', 'moderator')
+    @commands.has_any_role("admin", "moderator")
     async def stopsignup(self, ctx):
         self.toggle = False
         self.uid_list.clear()
@@ -35,60 +36,78 @@ class Attendance(commands.Cog):
         guild = self.bot.get_guild(self.bot.guilds[0].id)
         fng = discord.utils.get(guild.roles, name="fng")
         if fng in ctx.message.author.roles:
-            await ctx.message.add_reaction('👎')
+            await ctx.message.add_reaction("👎")
             return
         uid = ctx.message.author.id
         if uid in self.uid_list:
-            await ctx.message.add_reaction('👎')
+            await ctx.message.add_reaction("👎")
             return
         roles = []
         for arg in args:
-            roles.append(arg.replace(',','').strip(' '))
+            roles.append(arg.replace(",", "").strip(" "))
         if len(roles) == 0:
-            roles = ['']
+            roles = [""]
         roles = roles[slice(0, min(3, len(roles)))]
-        self.date = datetime.now(timezone('US/Eastern'))
+        self.date = datetime.now(timezone("US/Eastern"))
         user_data = {
-            'user_id' : ctx.message.author.id,
-            'nickname' : ctx.message.author.display_name,
-            'date' : self.date,
-            'roles' : roles,
+            "user_id": ctx.message.author.id,
+            "nickname": ctx.message.author.display_name,
+            "date": self.date,
+            "roles": roles,
         }
         await self.writer(user_data)
         self.uid_list.append(uid)
-        await ctx.message.add_reaction('👍')
+        await ctx.message.add_reaction("👍")
 
     async def writer(self, user_data):
-        await self.bot.conn.execute("""
+        await self.bot.conn.execute(
+            """
         INSERT INTO attendance (user_id, nickname, date)
         VALUES ($1, $2, $3)
-        """, user_data['user_id'], user_data['nickname'], user_data['date'])
-        for role in user_data['roles']:
-            await self.bot.conn.execute("""
+        """,
+            user_data["user_id"],
+            user_data["nickname"],
+            user_data["date"],
+        )
+        for role in user_data["roles"]:
+            await self.bot.conn.execute(
+                """
             INSERT INTO roles (attendance_id, role)
             VALUES ((SELECT id FROM attendance WHERE attendance.user_id = $1 
             AND attendance.date = $2), $3)
-            """, user_data['user_id'], user_data['date'], role)
+            """,
+                user_data["user_id"],
+                user_data["date"],
+                role,
+            )
 
     @commands.command()
-    @commands.has_any_role('admin', 'moderator')
+    @commands.has_any_role("admin", "moderator")
     async def remove(self, ctx):
         if self.toggle == False:
             return
         user_id = ctx.message.mentions[0].id
-        await self.bot.conn.execute("""
+        await self.bot.conn.execute(
+            """
         DELETE FROM roles WHERE attendance_id = (SELECT id FROM attendance WHERE attendance.user_id = $1 
         AND attendance.date = $2)
-        """, user_id, self.date)
-        await self.bot.conn.execute("""
+        """,
+            user_id,
+            self.date,
+        )
+        await self.bot.conn.execute(
+            """
         DELETE FROM attendance WHERE user_id = $1
-        AND date = $2""", user_id, self.date)
+        AND date = $2""",
+            user_id,
+            self.date,
+        )
         self.uid_list.remove(user_id)
-        await ctx.message.add_reaction('👍')
+        await ctx.message.add_reaction("👍")
 
     async def fake_signup(self, ctx):
         user = ctx.author
-        role = discord.utils.get(user.guild.roles, name='silenced')
+        role = discord.utils.get(user.guild.roles, name="silenced")
         await user.add_roles(role)
         await asyncio.sleep(30)
         await user.remove_roles(role)
@@ -96,14 +115,30 @@ class Attendance(commands.Cog):
     @commands.command()
     async def joined(self, ctx):
         user_id = ctx.author.id
-        result = await self.bot.conn.fetchrow("""
+        result = await self.bot.conn.fetchrow(
+            """
         SELECT join_date FROM date_joined WHERE user_id = $1;
-        """, user_id)
+        """,
+            user_id,
+        )
         if not result:
-            await ctx.message.add_reaction('👎')
+            await ctx.message.add_reaction("👎")
             return
         joined_date = result[0].strftime("%d %B, %Y")
         await ctx.send(joined_date)
+
+    @commands.command()
+    async def nolife(self, ctx):
+        result = await self.bot.conn.fetchrow(
+            """
+        SELECT COUNT(date) FROM attendance WHERE user_id = $1;
+        """,
+            ctx.author.id,
+        )
+        if result["count"] < 1:
+            await ctx.send("You haven't attended any Saturday Ops.")
+        else:
+            await ctx.send("Saturday Op Count: {}".format(result["count"]))
 
 
 def setup(bot):
