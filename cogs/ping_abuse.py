9682d9467b7ee8f42cb3edb65f9fd1fb52ef70cc
YouGotSchott <yougotschott@gmail.com>
2021-03-07 16:15:33 +0000

    Added 1 hour limit for ping abuse messages

--- cogs/ping_abuse.py
+++ cogs/ping_abuse.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from time import strftime
 
 
-class BaconBuster(commands.Cog):
+class PingAbuse(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -14,11 +14,13 @@ class BaconBuster(commands.Cog):
         msg = str(message.content)
         msg = msg.strip()
         if message.mentions:
-            log_time = datetime.now(timezone('US/Eastern')).strftime("%m/%d/%Y %H:%M:%S")
-            channel = discord.utils.get(self.bot.get_all_channels(), name='logging')
-            await channel.send((f'{log_time} - **{message.author.mention}** deleted a user mention in {message.channel.mention} | '
-                    f'**Message Content:** "{msg}"'))
+            time_elapsed = datetime.now() - message.created_at
+            if time_elapsed.total_seconds() <= 3600:
+                log_time = datetime.now(timezone('US/Eastern')).strftime("%m/%d/%Y %H:%M:%S")
+                channel = discord.utils.get(self.bot.get_all_channels(), name='logging')
+                await channel.send((f'{log_time} - **{message.author.mention}** deleted a user mention in {message.channel.mention} | '
+                        f'**Message Content:** "{msg}"'))
 
 
 def setup(bot):
-    bot.add_cog(BaconBuster(bot))
+    bot.add_cog(PingAbuse(bot))
