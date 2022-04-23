2e2b02ec68e475626e5a9edc8c8a82a1fc1ba392
lucas.schott <lucschott@gmail.com>
2022-04-23 13:25:17 +0000

    Added servers to info command

--- cogs/info.py
+++ cogs/info.py
@@ -11,28 +11,79 @@ class Info(commands.Cog):
             "https://s3.amazonaws.com/files.enjin.com/1015535/site_logo/2020_logo.png"
         )
 
-    @commands.command()
-    async def armaserver(self, ctx):
+    @commands.group()
+    async def info(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.message.add_reaction("👎")
+
+    # Modern Info
+    @info.group()
+    async def modern(self, ctx):
+        if ctx.invoked_subcommand is None:
+            em = discord.Embed(
+                title="Modern Arma Server - Direct Connect Info",
+                description=f"```IP: {server_info['main_host']}\nPassword: {server_info['main_pass']}\nPort: {server_info['modern_port']}```",
+                color=0x383F35,
+            )
+            em.set_thumbnail(url=self.thumbnail)
+            await ctx.channel.send(embed=em)
+
+    @modern.command(name="test")
+    async def modern_test(self, ctx):
         em = discord.Embed(
-            title="Arma Server - Direct Connect Info",
-            description=f"```IP: {server_info['main_host']}\nPassword: {server_info['main_pass']}\nPort: {server_info['main_port']}```",
+            title="Modern Mission Making Server - Direct Connect Info",
+            description=f"```IP: {server_info['main_host']}\nPassword: {server_info['test_pass']}\nPort: {server_info['modern_test_port']}```",
             color=0x383F35,
         )
         em.set_thumbnail(url=self.thumbnail)
         await ctx.channel.send(embed=em)
 
-    @commands.command()
-    async def testserver(self, ctx):
+    # Cold War Info
+    @info.group()
+    async def coldwar(self, ctx):
+        if ctx.invoked_subcommand is None:
+            em = discord.Embed(
+                title="Cold War Arma Server - Direct Connect Info",
+                description=f"```IP: {server_info['main_host']}\nPassword: {server_info['main_pass']}\nPort: {server_info['cw_port']}```",
+                color=0x383F35,
+            )
+            em.set_thumbnail(url=self.thumbnail)
+            await ctx.channel.send(embed=em)
+
+    @coldwar.command(name="test")
+    async def coldwar_test(self, ctx):
         em = discord.Embed(
-            title="Mission Making Server - Direct Connect Info",
-            description=f"```IP: {server_info['main_host']}\nPassword: {server_info['test_pass']}\nPort: {server_info['test_port']}```",
+            title="Cold War Mission Making Server - Direct Connect Info",
+            description=f"```IP: {server_info['main_host']}\nPassword: {server_info['test_pass']}\nPort: {server_info['cw_test_port']}```",
             color=0x383F35,
         )
         em.set_thumbnail(url=self.thumbnail)
         await ctx.channel.send(embed=em)
 
-    @commands.command()
-    async def mcserver(self, ctx):
+    # WW2 Info
+    @info.group()
+    async def ww2(self, ctx):
+        if ctx.invoked_subcommand is None:
+            em = discord.Embed(
+                title="World War II Arma Server - Direct Connect Info",
+                description=f"```IP: {server_info['main_host']}\nPassword: {server_info['main_pass']}\nPort: {server_info['ww2_port']}```",
+                color=0x383F35,
+            )
+            em.set_thumbnail(url=self.thumbnail)
+            await ctx.channel.send(embed=em)
+
+    @ww2.command(name="test")
+    async def ww2_test(self, ctx):
+        em = discord.Embed(
+            title="World War II Mission Making Server - Direct Connect Info",
+            description=f"```IP: {server_info['main_host']}\nPassword: {server_info['test_pass']}\nPort: {server_info['ww2_test_port']}```",
+            color=0x383F35,
+        )
+        em.set_thumbnail(url=self.thumbnail)
+        await ctx.channel.send(embed=em)
+
+    @info.command()
+    async def minecraft(self, ctx):
         em = discord.Embed(
             title="Minecraft Server - Direct Connect Info",
             description=f"```IP: {server_info['mc_host']}```",
@@ -41,7 +92,7 @@ class Info(commands.Cog):
         em.set_thumbnail(url=self.thumbnail)
         await ctx.channel.send(embed=em)
 
-    @commands.command()
+    @info.command()
     async def ts3(self, ctx):
         em = discord.Embed(
             title="TS3 - Direct Connect Info",
@@ -51,7 +102,7 @@ class Info(commands.Cog):
         em.set_thumbnail(url=self.thumbnail)
         await ctx.channel.send(embed=em)
 
-    @commands.command()
+    @info.command()
     async def ocap(self, ctx):
         em = discord.Embed(
             title="OCAP URL",
